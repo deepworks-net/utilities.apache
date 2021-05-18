4f48f035db0f5f2d0c3fb993b182ef24c140e836
Matthew Buske <matt@deepworks.net>
2021-05-18 18:35:29 -0400

    Cleanup Fixes
    Fixed some issues with cleanup

--- scripts/build/httpd/cleanup.sh
+++ scripts/build/httpd/cleanup.sh
@@ -9,7 +9,8 @@ set -e
 echo "Cleaning Up Apache Install (Removing development/compilation files)" 
 
 # Remove the Apache Header Files as they are no longer needed... (Removes ~2 MB - basically httpd-dev)
-rm -r "$includedir" && mkdir -p "$includedir" && rm -r "$iconsdir" && mkdir -p "$iconsdir"
+rm -rf "$includedir" && mkdir -p "$includedir" 
+rm -rf "$iconsdir" && mkdir -p "$iconsdir"
 
 # Also removing the .a files (like ~1 MB) as they are only needed during compilation (apr and apr-util)
 rm -f "$prefix/lib/libapr-1.a" && rm -f "$prefix/lib/libaprutil-1.a"
--- scripts/build/openssl/cleanup.sh
+++ scripts/build/openssl/cleanup.sh
@@ -9,7 +9,7 @@ set -e
 echo "Cleaning Up OpenSSL Install (Removing development/compilation files)" 
 
 # Remove the OpenSSL Header Files as they are no longer needed... (Removes 1.5 MB - basically OpenSSL-Dev)
-rm -r "$ssl_prefix/include" && rm "$ssl_prefix"/bin/c_rehash
+rm -rf "$ssl_prefix/include" && rm -rf "$ssl_prefix"/bin/c_rehash
 
 # Also removing the .a files (like 6+ MB) as they are only needed during compilation
 rm -f "$ssl_prefix/lib/libcrypto.a" && rm -f "$ssl_prefix/lib/libssl.a"
